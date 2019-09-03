edf8f08d692f84195f9eacdda2914e99f293777f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-09-03 22:09:01 +0100

    r Separate out some classes, for re-use

--- demos/google_test_demo/Test07TestScenarios.cpp
+++ demos/google_test_demo/Test07TestScenarios.cpp
@@ -1,9 +1,12 @@
-#include <ostream>
 #include "ApprovalTests.hpp"
+
+#include "../../include/FibonacciCalculator.h"
+#include "../../include/DateRemovingLogFileWriter.h"
+
 #include <gtest/gtest.h>
-#include <chrono>
-#include <ctime>
-#include <regex>
+
+#include <ostream>
+
 
 using namespace ApprovalTests;
 
@@ -40,115 +43,6 @@ TEST(Test07TestScenarios, New_test_of_legacy_feature)
 //--------------------------------------------------------------------------------
 
 // Problem: Output contains data that changes each run, e.g. date-and-time
-
-namespace
-{
-    class FibonacciCalculator
-    {
-    public:
-        explicit FibonacciCalculator(unsigned value) : value(value)
-        {
-        }
-
-        void calculate(const std::string& path)
-        {
-            std::ofstream stream(path);
-
-            stream << "f(" << value << ") = " << fibonacci(value) << '\n';
-            auto end = std::chrono::system_clock::now();
-            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-            stream << "finished computation at " << std::ctime(&end_time) << '\n';
-        }
-
-    private:
-        static long fibonacci(unsigned n)
-        {
-            if (n < 2) return n;
-            return fibonacci(n-1) + fibonacci(n-2);
-        }
-
-        unsigned value;
-    };
-
-    class DateRemovingLogFileWriter : public ApprovalWriter
-    {
-    public:
-        explicit DateRemovingLogFileWriter(FibonacciCalculator& calculator) : calculator(calculator)
-        {
-        }
-        std::string getFileExtensionWithDot() const override
-        {
-            return ".log";
-        }
-
-        void write(std::string path) const override
-        {
-            calculator.calculate(path);
-            rewriteLogFileRemovingDatesAndTimes(path);
-        }
-
-        void cleanUpReceived(std::string receivedPath) const override
-        {
-            ::remove(receivedPath.c_str());
-        }
-
-    private:
-        void rewriteLogFileRemovingDatesAndTimes(const std::string& path) const
-        {
-            auto lines = readLines(path);
-            stripDatesAndTimes(lines);
-            writeLines(lines, path);
-        }
-
-        std::vector<std::string> readLines(const std::string& path ) const
-        {
-            std::ifstream infile(path);
-            if( !infile.is_open() )
-                throw std::domain_error( "Unable to load input file: " + path );
-
-            std::vector<std::string> lines;
-            std::string line;
-            while (std::getline(infile, line))
-            {
-                lines.emplace_back(line);
-            }
-            return lines;
-        }
-
-        std::string stripDateAndTime(const std::string& line) const
-        {
-            // Example date:
-            // Tue Sep  3 16:58:52 2019
-            const auto dateRegex = R"(([A-Za-z]{3}) ([A-Za-z]{3}) ([0-9 ]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) ([0-9]{4}))";
-            const std::string replacementText = "[date-time-removed]";
-            return std::regex_replace(line, std::regex(dateRegex), replacementText);
-        }
-
-        void stripDatesAndTimes(std::vector<std::string>& lines) const
-        {
-            for(auto& line : lines)
-            {
-                line = stripDateAndTime(line);
-            }
-        }
-
-        void writeLines(const std::vector<std::string>& lines, const std::string& path ) const
-        {
-            std::ofstream outfile(path);
-            if( !outfile.is_open() )
-                throw std::domain_error( "Unable to re-write input file: " + path );
-
-            for( const auto& line : lines)
-            {
-                outfile << line << '\n';
-            }
-        }
-
-    private:
-        FibonacciCalculator& calculator;
-    };
-}
 
 TEST(Test07TestScenarios, Deal_with_dates_and_times_in_output)
 {
