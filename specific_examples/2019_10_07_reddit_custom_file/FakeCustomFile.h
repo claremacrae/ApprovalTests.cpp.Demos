820e38f5feb52a63267a3eb57554d62e2a6cef66
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:19:07 +0100

    r Tidy up white space

--- specific_examples/2019_10_07_reddit_custom_file/FakeCustomFile.h
+++ specific_examples/2019_10_07_reddit_custom_file/FakeCustomFile.h
@@ -8,10 +8,10 @@ class custom_file
 public:
     explicit custom_file(const std::string&);
     std::string size() const;
-        
+
     int header1 = 42;
     std::string header2 = "FOO";
-        
+
 };
 
 #endif //APPROVALTESTS_CPP_FAKECUSTOMFILE_H
