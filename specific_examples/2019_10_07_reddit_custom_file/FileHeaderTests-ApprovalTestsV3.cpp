88ad4675e66b66b52420119d5a5273a8dfa1bb6d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:32:06 +0100

    c Add explanatory comments

--- specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV3.cpp
+++ specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV3.cpp
@@ -14,10 +14,15 @@ struct WriteHeaders
         os << "size:    " << wrapper.test_file.size() << '\n';
         os << "header1: " << wrapper.test_file.header1 << '\n';
         os << "header2: " << wrapper.test_file.header2 << '\n';
+        // ... and so on ...
         return os;
     }
 };
 
+// In this version, we provide a helper function that takes a file name,
+// reads it, and verifies it.
+// This aids readability of the test, and makes it easier to test
+// multiple files.
 void verify_custom_file_headers(const std::string& filename)
 {
     using namespace ApprovalTests;
@@ -29,3 +34,9 @@ TEST_CASE( "header fields are correct - with function to test headers", "[header
 {
     verify_custom_file_headers("filename");
 }
+
+// As an extension to this, you could write multiple different tests,
+// each reading a different file.
+// Or you could make this one test read multiple files, using one of the 
+// approaches in:
+//      https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/MultipleOutputFilesPerTest.md#top
