e8498cf26052296d76af0c7fbb0d5df50d1209ea
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:27:51 +0100

    c Add explanatory comments

--- specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV2.cpp
+++ specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV2.cpp
@@ -2,6 +2,10 @@
 #include "Catch.hpp"
 #include "FakeCustomFile.h"
 
+// In this version, we provide a class to write out just the information
+// that we intend to test here, i.e. the headers.
+// This gives us more flexibility than in the previous example, which used
+// an output stream operator.
 struct WriteHeaders
 {
     explicit WriteHeaders(const custom_file& test_file) : test_file(test_file)
@@ -14,6 +18,7 @@ struct WriteHeaders
         os << "size:    " << wrapper.test_file.size() << '\n';
         os << "header1: " << wrapper.test_file.header1 << '\n';
         os << "header2: " << wrapper.test_file.header2 << '\n';
+        // ... and so on ...
         return os;
     }
 };
