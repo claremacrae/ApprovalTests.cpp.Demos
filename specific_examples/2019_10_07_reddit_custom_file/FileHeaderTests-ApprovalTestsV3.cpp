fd1ca7a542df4828cdece641238c33a2c922ab53
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:38:54 +0100

    t Include the file name in output of example 3

--- specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV3.cpp
+++ specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-ApprovalTestsV3.cpp
@@ -2,18 +2,25 @@
 #include "Catch.hpp"
 #include "FakeCustomFile.h"
 
+// This extends the previous version, this time including the file name in 
+// the text being verified, which would be useful if testing the reading
+// of multiple files.
 struct WriteHeaders
 {
-    explicit WriteHeaders(const custom_file& test_file) : test_file(test_file)
+    explicit WriteHeaders(
+        const std::string& filename,
+        const custom_file& test_file) : filename(filename), test_file(test_file)
     {
     }
 
+    const std::string& filename;
     const custom_file& test_file;
 
     friend std::ostream &operator<<(std::ostream &os, const WriteHeaders& wrapper) {
-        os << "size:    " << wrapper.test_file.size() << '\n';
-        os << "header1: " << wrapper.test_file.header1 << '\n';
-        os << "header2: " << wrapper.test_file.header2 << '\n';
+        os << "filename: " << wrapper.filename << '\n';
+        os << "size:     " << wrapper.test_file.size() << '\n';
+        os << "header1:  " << wrapper.test_file.header1 << '\n';
+        os << "header2:  " << wrapper.test_file.header2 << '\n';
         // ... and so on ...
         return os;
     }
@@ -26,8 +33,8 @@ struct WriteHeaders
 void verify_custom_file_headers(const std::string& filename)
 {
     using namespace ApprovalTests;
-    custom_file big_file{"filename"};
-    Approvals::verify( WriteHeaders(big_file) );
+    custom_file big_file{filename};
+    Approvals::verify( WriteHeaders(filename, big_file) );
 }
 
 TEST_CASE( "header fields are correct - with function to test headers", "[header]" )
