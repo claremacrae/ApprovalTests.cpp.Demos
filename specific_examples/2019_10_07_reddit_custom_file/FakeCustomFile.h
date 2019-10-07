5594e9aeda3d7f4bcfa5c2d1556ad58de3fb3388
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:05:14 +0100

    r Move implementations to .cpp file

--- specific_examples/2019_10_07_reddit_custom_file/FakeCustomFile.h
+++ specific_examples/2019_10_07_reddit_custom_file/FakeCustomFile.h
@@ -6,14 +6,8 @@
 class custom_file
 {
 public:
-    explicit custom_file(const std::string&)
-    {
-        // Ignore filename for now - just use hard-coded values for testing
-    }
-    std::string size() const
-    {
-        return "100MB";
-    }
+    explicit custom_file(const std::string&);
+    std::string size() const;
         
     int header1 = 42;
     std::string header2 = "FOO";
