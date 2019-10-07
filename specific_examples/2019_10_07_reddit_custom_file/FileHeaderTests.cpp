ed6cae9da1a4d75bee89224eb7d927cda294b622
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 10:59:32 +0100

    First version of demo code to help Reddit answer
    https://old.reddit.com/r/cpp_questions/comments/de5ojy/how_should_i_perform_a_setup_in_catch2/

---
+++ specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests.cpp
@@ -0,0 +1,82 @@
+#include "ApprovalTests.hpp"
+#include "Catch.hpp"
+
+class custom_file
+{
+public:
+    custom_file(const std::string&)
+    {
+        // Ignore filename for now - just use hard-coded values for testing
+    }
+    std::string size() const
+    {
+        return "100MB";
+    }
+        
+    int header1 = 42;
+    std::string header2 = "FOO";
+        
+};
+
+TEST_CASE( "header fields are correct", "[header]" ){
+    custom_file big_file{"filename"};
+    REQUIRE(big_file.size() == "100MB");
+
+    SECTION("header1 should be 42") {
+        REQUIRE(big_file.header1 == 42);
+    }
+
+    SECTION("header2 should be FOO") {
+        REQUIRE(big_file.header2 == "FOO");
+    }
+}
+
+//std::ostream &operator<<(std::ostream &os, const custom_file& test_file)
+//{
+//    os << "size:    " << test_file.size() << '\n';
+//    os << "header1: " << test_file.header1 << '\n';
+//    os << "header2: " << test_file.header2 << '\n';
+//    return os;
+//}
+//
+//TEST_CASE( "header fields are correct - with Approval Tests 1", "[header]" )
+//{
+//    using namespace ApprovalTests;
+//    custom_file big_file{"filename"};
+//    Approvals::verify(big_file);
+//}
+
+struct WriteHeaders
+{
+    explicit WriteHeaders(const custom_file& test_file) : test_file(test_file)
+    {
+    }
+
+    const custom_file& test_file;
+
+    friend std::ostream &operator<<(std::ostream &os, const WriteHeaders& wrapper) {
+        os << "size:    " << wrapper.test_file.size() << '\n';
+        os << "header1: " << wrapper.test_file.header1 << '\n';
+        os << "header2: " << wrapper.test_file.header2 << '\n';
+        return os;
+    }
+};
+
+TEST_CASE( "header fields are correct - with Approval Tests 2", "[header]" )
+{
+    using namespace ApprovalTests;
+    custom_file big_file{"filename"};
+    Approvals::verify( WriteHeaders(big_file) );
+}
+
+void verify_custom_file_headers(const std::string& filename)
+{
+    using namespace ApprovalTests;
+    custom_file big_file{"filename"};
+    Approvals::verify( WriteHeaders(big_file) );
+}
+
+TEST_CASE( "header fields are correct - with Approval Tests 3", "[header]" )
+{
+    verify_custom_file_headers("filename");
+}
---
+++ specific_examples/2019_10_07_reddit_custom_file/README.md
@@ -0,0 +1,3 @@
+# Demo code for Reddit question
+
+Demo code to help show Approval Tests approach for this question: [How should I perform a setup in Catch2?](https://old.reddit.com/r/cpp_questions/comments/de5ojy/how_should_i_perform_a_setup_in_catch2/)
