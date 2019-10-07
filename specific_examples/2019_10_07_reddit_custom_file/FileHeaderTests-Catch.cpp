4a17530408df7427bb8cada4c87ae9228101ab8d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-07 11:25:56 +0100

    c Add explanatory comments

--- specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-Catch.cpp
+++ specific_examples/2019_10_07_reddit_custom_file/FileHeaderTests-Catch.cpp
@@ -1,6 +1,8 @@
 #include "Catch.hpp"
 #include "FakeCustomFile.h"
 
+// Initial test  code from
+// https://old.reddit.com/r/cpp_questions/comments/de5ojy/how_should_i_perform_a_setup_in_catch2/
 TEST_CASE( "header fields are correct - pure catch", "[header]" ){
     custom_file big_file{"filename"};
     REQUIRE(big_file.size() == "100MB");
