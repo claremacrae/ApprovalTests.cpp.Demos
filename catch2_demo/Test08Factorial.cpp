381a17fec9b86c0adf31be8b023c8d84ed263772
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-21 13:19:41 +0000

    t Add example of verifyAll() with Catch2 tutorial's Factorial function

---
+++ catch2_demo/Test08Factorial.cpp
@@ -0,0 +1,25 @@
+#include "ApprovalTests.hpp"
+#include "Catch.hpp"
+
+#include <numeric>
+
+using namespace ApprovalTests;
+
+int Factorial( int number )
+{
+    return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+}
+
+TEST_CASE( "Factorials up to 10" )
+{
+    // Create a container with values 0 ... 10 inclusive
+    std::vector<int> inputs(11);
+    std::iota(inputs.begin(), inputs.end(), 0);
+
+    // Act on all values in inputs container:
+    Approvals::verifyAll("Factorial", inputs,
+        [](auto i, auto& os)
+        {
+            os << i << "! => " << Factorial(i);
+        });
+}
