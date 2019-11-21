b6a09cc4e702c581ae51cc04df6d33014b399dd4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-21 08:40:33 +0000

    r Apply .clang-format layout to GildedRoseApprovalTests.cc for readability of the lambda

--- specific_examples/2019_11_18_fuzzing_gilded_rose/GildedRoseApprovalTests.cc
+++ specific_examples/2019_11_18_fuzzing_gilded_rose/GildedRoseApprovalTests.cc
@@ -6,24 +6,28 @@ using namespace ApprovalTests;
 
 std::ostream& operator<<(std::ostream& os, const Item& obj)
 {
+    // clang-format off
     return os
         << "name: " << obj.name
         << ", sellIn: " << obj.sellIn
         << ", quality: " << obj.quality;
+    // clang-format on
 }
 
 TEST(GildedRoseApprovalTests, VerifyCombinations)
 {
-    std::vector<std::string> names { "Foo" };
-    std::vector<int> sellIns { 1 };
-    std::vector<int> qualities { 1 };
+    std::vector<std::string> names{"Foo"};
+    std::vector<int> sellIns{1};
+    std::vector<int> qualities{1};
 
     CombinationApprovals::verifyAllCombinations(
-                [](std::string name, int sellIn, int quality) {
-                  std::vector<Item> items = {Item(name, sellIn, quality)};
-                GildedRose app(items);
-                app.updateQuality();
-                return items[0];
-                },
-                names, sellIns, qualities);
+        [](std::string name, int sellIn, int quality) {
+            std::vector<Item> items = {Item(name, sellIn, quality)};
+            GildedRose app(items);
+            app.updateQuality();
+            return items[0];
+        },
+        names,
+        sellIns,
+        qualities);
 }
