642b8e30c99fb8f82739e443ce509d03d0720a3f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-08 15:50:40 +0100

    Add some explanatory comments.

--- specific_examples/2020_04_08_reusing_main_customisations/OurApprovalTestCustomisations.h
+++ specific_examples/2020_04_08_reusing_main_customisations/OurApprovalTestCustomisations.h
@@ -1,6 +1,17 @@
 #ifndef APPROVALTESTS_CPP_OURAPPROVALTESTCUSTOMISATIONS_H
 #define APPROVALTESTS_CPP_OURAPPROVALTESTCUSTOMISATIONS_H
 
+// The constructor of this class sets up one or more Approval Tests.
+// By storing the returned "disposer" (RAII) objects as data members,
+// the customisations remain in effect until the object is destroyed.
+//
+// This is useful if you have multiple test programs, and you want consistent
+// customisations in all of them. You can put a class like this in a library
+// of code that is shared between all the test programs, and have one of
+// these objects created in the main.cpp of each test.
+//
+// Then, if you later wish to add change or add a customisation, you only
+// need to update this one class.
 class OurApprovalTestCustomisations
 {
 public:
--- specific_examples/2020_04_08_reusing_main_customisations/main.cpp
+++ specific_examples/2020_04_08_reusing_main_customisations/main.cpp
@@ -4,4 +4,6 @@
 
 using namespace ApprovalTests;
 
+// This object is created before main() starts, and it sets up all the
+// Approval Tests customisations that are to be applied to all tests.
 OurApprovalTestCustomisations customisations;
