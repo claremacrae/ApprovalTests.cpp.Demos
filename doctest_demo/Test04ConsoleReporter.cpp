5a47328db06642c1c29f106d298867301983899d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-08 15:02:10 +0100

    Update code to work with ApprovalTests v.8.5.0 We no longer need to provide our own ConsoleDiffReporter, as Approval Tests provides this now.

--- doctest_demo/Test04ConsoleReporter.cpp
+++ doctest_demo/Test04ConsoleReporter.cpp
@@ -1,8 +1,6 @@
 #include "ApprovalTests.hpp"
 #include "doctest/doctest.h"
 
-#include "include/ConsoleDiffReporter.h"
-
 using namespace ApprovalTests;
 
 TEST_CASE("UseConsoleReporter" * doctest::should_fail(true))
@@ -11,14 +9,5 @@ TEST_CASE("UseConsoleReporter" * doctest::should_fail(true))
     // output reporting the test failure.
     // However, catch2 separates the two, so that the diff output appears a long
     // way before the FAILED output.
-    FirstWorkingReporter diff_reporter(
-        {
-            // On Windows, SystemLauncher does not yet know how to use the "where" command to find
-            // the location of a program that is in the PATH, so we have to give the full
-            // path to fc.exe here:
-            new ConsoleDiffReporter("C:\\Windows\\System32\\fc.exe"),
-            // On Linux and Mac, this one will be used:
-            new ConsoleDiffReporter("diff")
-        });
-    Approvals::verify("Some\nMulti-line\noutput", diff_reporter);
+    Approvals::verify("Some\nMulti-line\noutput", TextDiffReporter());
 }
