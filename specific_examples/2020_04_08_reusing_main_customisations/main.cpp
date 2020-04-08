7f4e8fbe0976ac1557f1b459d5abf7ca38e83fd4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-04-08 15:52:52 +0100

    Explain how to use with a custom main()

--- specific_examples/2020_04_08_reusing_main_customisations/main.cpp
+++ specific_examples/2020_04_08_reusing_main_customisations/main.cpp
@@ -7,3 +7,7 @@ using namespace ApprovalTests;
 // This object is created before main() starts, and it sets up all the
 // Approval Tests customisations that are to be applied to all tests.
 OurApprovalTestCustomisations customisations;
+
+// Note that if you have a custom main() function, you can create
+// the customisations object inside your main(), before all the tests
+// are run.
