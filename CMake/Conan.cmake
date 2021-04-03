ed02efcb65b11a37224df6889c36407a5a0d18fe
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-04-03 21:58:07 +0100

    Remove unused bintray reference
    This is being turned off soon, and all our dependencies
    are being obtained from conan-center-index

--- CMake/Conan.cmake
+++ CMake/Conan.cmake
@@ -10,9 +10,6 @@ endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-conan_add_remote(NAME bincrafters URL
-                 https://api.bintray.com/conan/bincrafters/public-conan)
-
 conan_cmake_run(
   CONANFILE conanfile.txt
   BASIC_SETUP
